8738d92f9da2b20414191d0b95ae1621fc37bb0e
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2020-10-21 01:19:19 -0700

    Add files via upload

---
+++ tie drawing.py
@@ -0,0 +1,25 @@
+# Tie
+
+import turtle
+
+t = turtle.Turtle()
+
+t.left(45)
+t.forward(80)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(80)
+
+t.left(45)
+t.right(75)
+t.forward(220)
+t.right(60)
+t.forward(80)
+t.right(90)
+t.forward(80)
+t.right(60)
+t.forward(220)
+
